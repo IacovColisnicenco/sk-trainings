452786745c3282e105d54d3876ab0300e761f260
Yakov <jacobwebm@gmail.com>
2021-03-16 22:09:59 +0200

    training13 - js intro

---
+++ app/js/script.js
@@ -0,0 +1,16 @@
+let i = 0;
+/* const j = 0; */
+console.log(typeof i);
+
+/* i = "hello";
+console.log(typeof i); */
+//j = 1;
+/* alert(j); */
+
+
+if (i  === '0') { /// === - сравнение по типу строгое
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+  
